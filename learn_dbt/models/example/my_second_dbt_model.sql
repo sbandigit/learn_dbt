b5c7cf60825844b7e756a4ff8f4ed30c3c9013bd
subbu <subbu.varsith1@gmail.com>
2025-06-20 12:03:20 -0400

    renamed second model

--- learn_dbt/models/example/my_second_dbt_model.sql
+++ learn_dbt/models/example/my_second_dbt_model.sql
@@ -1,10 +1,11 @@
 
 -- Use the `ref` function to select from other models
-
-select *
-from {{ ref('my_first_dbt_model') }}
---where id = 1
--- UNION ALL
--- SELECT 
---  4 AS ID,
---  'USER4' AS USER
+{{config(materialized='table',alias='second_model')}}
+SELECT 
+ *
+FROM  
+ {{ ref('my_first_dbt_model') }}
+UNION ALL
+SELECT 
+ 4 AS ID,
+ 'USER4' AS USER
