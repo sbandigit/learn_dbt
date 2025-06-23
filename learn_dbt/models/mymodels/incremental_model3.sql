8dd0b0014b0aba7078d63f0466dd3d307bcab14c
subbu <subbu.varsith1@gmail.com>
2025-06-23 15:02:00 -0400

    added date_dim_new model

---
+++ learn_dbt/models/mymodels/incremental_model3.sql
@@ -0,0 +1,11 @@
+{{config(materialized='incremental', unique_key='d_date',alias='date_dim_new')}}
+
+SELECT
+ *
+FROM 
+ {{source('SF10TCL','DATE_DIM')}}
+WHERE D_DATE <= CURRENT_DATE
+
+{% if is_incremental()%}
+ AND D_DATE> (SELECT MAX(D_DATE) FROM {{this}})
+{% endif %}
